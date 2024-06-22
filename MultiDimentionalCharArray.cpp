10060247eb3a89e19e2f205b52c6cea9b97ac68a
Abid Ali <117859453+Abid-Ali-01@users.noreply.github.com>
2024-06-22 21:53:41 +0500

    Add files via upload

---
+++ MultiDimentionalCharArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main (){
+
+
+string info [10][30] = {
+        {"Quetta", "Capital of Balochistan"},
+        {"Karachi", "Capital of Sindh"},
+        {"Peshawar", "Capital of KPK"},
+        {"Lahore", "Capital of Punjab"},
+        {"Islamabad", "Capital of Pakistan"}};
+
+cout<<left <<setw(12)<<"City"<<"Info"<<endl;
+cout<<left <<setw(10)<<"========== "<<" ======================="<<endl;
+for(int i =0 ; i<=5; i++){
+
+cout<<left <<setw(12) <<info[i][0] <<info[i][1]<<endl;
+
+}
+
+}
